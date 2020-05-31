82ed3d9ca217dabc07197788dbd5cc8b6cd7af78
Odimaa <odimaa.horsfall@gmail.com>
2020-05-31 10:34:22 +0100

    second push

---
+++ main.js
@@ -0,0 +1,14 @@
+
+/*new for me*/
+const selectElement = function (element) {
+    return document.querySelector(element);
+};
+
+
+let menuToggler = selectElement('.menu-toggle');
+let body = selectElement('body');
+
+/*new for me*/
+menuToggler.addEventListener('click', function () {
+    body.classList.toggle('open');
+});
